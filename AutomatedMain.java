2ea426df4f3672c5637547afc61aad1d1dbf3e4e
Shuvajit Barua <59844915+shuvajitB@users.noreply.github.com>
2022-02-20 01:13:36 +0600

    Initial Commit

--- AutomatedMain.java
+++ AutomatedMain.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.edge.EdgeDriver;
 
 
 
-public class FirstAccount{
+public class AutomatedMain{
 
     public static void main(String[] args)  {
         System.setProperty("webdriver.edge.driver","C:\\Users\\shuva\\OneDrive\\Documents\\Browser driver\\msedgedriver.exe" );
